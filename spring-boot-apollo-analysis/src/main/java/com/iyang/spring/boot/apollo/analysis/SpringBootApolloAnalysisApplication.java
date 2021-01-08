ccf8e8cdae09e58f5ec76b13c58d8f86ba09d0fb
baoyang23 <1411091515@qq.com>
2021-01-08 18:06:12 +0800

    feat: commit boot with apollo project.

---
+++ spring-boot-apollo-analysis/src/main/java/com/iyang/spring/boot/apollo/analysis/SpringBootApolloAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.iyang.spring.boot.apollo.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApolloAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApolloAnalysisApplication.class, args);
+    }
+
+}
---
+++ spring-boot-apollo-analysis/src/test/java/com/iyang/spring/boot/apollo/analysis/SpringBootApolloAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iyang.spring.boot.apollo.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApolloAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
